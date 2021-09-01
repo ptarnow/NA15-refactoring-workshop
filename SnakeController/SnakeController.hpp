bb67ec38180e0940cfd2a62c8f0509934c8a8fd5
Piotr Tarnowski <ptarnowski4@gmail.com>
2021-09-01 17:56:04 +0200

    x

--- SnakeController/SnakeController.hpp
+++ SnakeController/SnakeController.hpp
@@ -49,6 +49,10 @@ private:
 
     Direction m_currentDirection;
     std::list<Segment> m_segments;
+    
+    public:
+    bool checkCrashWithSnake(Segment newHead);
+    bool checkOutOfMap(Segment newHead);
 };
 
 } // namespace Snake
